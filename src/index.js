584a7632e0224cf6c8d80488ffdb38e9afb5fe0e
Ishaan Phaye <ishaanphaye@gmail.com>
2026-06-16 23:53:28 +0530

    update

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use(express.json());
 
 // Base Health Check Route
-app.get('/', (req, res) => {
+app.get('/api/health', (req, res) => {
   res.status(200).json({
     success: true,
     message: 'Welcome to Vaatika Zone API Server (Chhattisgarh Fruits & Vegetables)',
